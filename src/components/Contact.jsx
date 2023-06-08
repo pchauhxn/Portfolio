596111f63e7ddd8fc8dad5f530784df89fc2233d
pchauhxn <101443722+pchauhxn@users.noreply.github.com>
2023-06-08 21:40:53 +0530

    Update Contact.jsx
    Input feild required function added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 text-2xl py-4'>Submit the form below or shoot me an email - pchauhan7289@gmail.com</p>
             </div>
-            <input className='bg-[#c8c9cd] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#c8c9cd]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#c8c9cd] p-2' name="message" rows="10" placeholder='Message'></textarea>
+            <input className='bg-[#c8c9cd] p-2' type="text" placeholder='Name' name='name' required/>
+            <input className='my-4 p-2 bg-[#c8c9cd]' type="email" placeholder='Email' name='email' required />
+            <textarea className='bg-[#c8c9cd] p-2' name="message" rows="10" placeholder='Message' required></textarea>
             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
